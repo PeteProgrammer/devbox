4eb0510dd4982c3c485624ab712cc3332c19f0b9
Peter Strøiman <peter@stroiman.com>
2017-01-24 09:12:54 +0100

    Install java

---
+++ install-java.sh
@@ -0,0 +1,5 @@
+if [ `id -u` -ne 0 ]; then
+  echo "Must be run as root"; exit 1
+fi
+
+sudo apt-get install default-jre default-jdk
--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ set -e
 VAGRANT_DIR=/vagrant
 $VAGRANT_DIR/install-docker.sh
 $VAGRANT_DIR/install-apps.sh
+$VAGRANT_DIR/install-java.sh
