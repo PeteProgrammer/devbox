612c81e71204b6ff33ab9b60c27bbfe3186c4425
Peter StrÃ¸iman <peter@stroiman.com>
2017-05-23 09:07:08 +0200

    Vim installation fun

--- install-apps.sh
+++ install-apps.sh
@@ -6,9 +6,15 @@ fi
 
 apt-get update
 apt-get install -y dos2unix curl
-apt-get install -y build-essential cmake
+apt-get install -y build-essential cmake python-dev python-pip python3-dev python3-pip
 
 # vim 8.0
-add-apt-repository ppa:jonathonf/vim
+add-apt-repository -y ppa:jonathonf/vim
 apt-get update
-apt-get install vim vim-gtk
+apt-get install -y vim vim-gtk
+
+# neovim
+add-apt-repository -y ppa:neovim-ppa/unstable
+apt-get update
+apt-get -y install software-properties-common
+apt-get -y install neovim
